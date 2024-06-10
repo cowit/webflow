41a2cc73b425ba23ce289448440f0d6a78c77607
Cowit <cowitdev@gmail.com>
2024-06-10 14:23:53 -0500

    Fixed button toggle

--- darkmodeswapper.js
+++ darkmodeswapper.js
@@ -33,42 +33,38 @@ function colorModeToggle() {
   if (!primaryTheme) {
     console.warn(`Value of primary-theme not found. Setting to "color"`);
     primaryTheme = "color"
-    return;
   }
 
   var secondaryTheme = scriptTag.getAttribute("secondary-theme");
   if (!secondaryTheme) {
     console.log(`Value of secondary-theme not found. Setting to "secondary"`);
     secondaryTheme = "secondary"
-    return;
   }
 
-  var toggleButton =scriptTag.getAttribute("toggle-button");
+  var toggleButton = scriptTag.getAttribute("toggle-button");
   if (!toggleButton) {
     console.log(`Value of toggle button not found. Setting to "mode-toggle"`);
     toggleButton = "mode-toggle"
-    return;
   }
 
-  var primaryLogo =scriptTag.getAttribute("primary-logo");
+  var primaryLogo = scriptTag.getAttribute("primary-logo");
   if (!primaryLogo) {
-    console.log(`Value of secondary logo svg not found. Setting to sun svg`);
-    primaryLogo = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M361.5 1.2c5 2.1 8.6 6.6 9.6 11.9L391 121l107.9 19.8c5.3 1 9.8 4.6 11.9 9.6s1.5 10.7-1.6 15.2L446.9 256l62.3 90.3c3.1 4.5 3.7 10.2 1.6 15.2s-6.6 8.6-11.9 9.6L391 391 371.1 498.9c-1 5.3-4.6 9.8-9.6 11.9s-10.7 1.5-15.2-1.6L256 446.9l-90.3 62.3c-4.5 3.1-10.2 3.7-15.2 1.6s-8.6-6.6-9.6-11.9L121 391 13.1 371.1c-5.3-1-9.8-4.6-11.9-9.6s-1.5-10.7 1.6-15.2L65.1 256 2.8 165.7c-3.1-4.5-3.7-10.2-1.6-15.2s6.6-8.6 11.9-9.6L121 121 140.9 13.1c1-5.3 4.6-9.8 9.6-11.9s10.7-1.5 15.2 1.6L256 65.1 346.3 2.8c4.5-3.1 10.2-3.7 15.2-1.6zM160 256a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zm224 0a128 128 0 1 0 -256 0 128 128 0 1 0 256 0z"/></svg>`
-    return;
+    console.log(`Value of secondary logo svg not found. Setting to moon svg`);
+    primaryLogo = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" aria-hidden="true" focusable="false" class="btn-icon"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"/></svg>`
   }
 
-  var secondaryLogo =scriptTag.getAttribute("secondary-logo");
+  var secondaryLogo = scriptTag.getAttribute("secondary-logo");
   if (!secondaryLogo) {
-    console.log(`Value of secondary logo svg not found. Setting to moon svg`);
-    secondaryLogo = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"/></svg>`
-    return;
+ 
+    console.log(`Value of secondary logo svg not found. Setting to sun svg`);
+    secondaryLogo = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" aria-hidden="true" focusable="false" class="btn-icon"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M361.5 1.2c5 2.1 8.6 6.6 9.6 11.9L391 121l107.9 19.8c5.3 1 9.8 4.6 11.9 9.6s1.5 10.7-1.6 15.2L446.9 256l62.3 90.3c3.1 4.5 3.7 10.2 1.6 15.2s-6.6 8.6-11.9 9.6L391 391 371.1 498.9c-1 5.3-4.6 9.8-9.6 11.9s-10.7 1.5-15.2-1.6L256 446.9l-90.3 62.3c-4.5 3.1-10.2 3.7-15.2 1.6s-8.6-6.6-9.6-11.9L121 391 13.1 371.1c-5.3-1-9.8-4.6-11.9-9.6s-1.5-10.7 1.6-15.2L65.1 256 2.8 165.7c-3.1-4.5-3.7-10.2-1.6-15.2s6.6-8.6 11.9-9.6L121 121 140.9 13.1c1-5.3 4.6-9.8 9.6-11.9s10.7-1.5 15.2 1.6L256 65.1 346.3 2.8c4.5-3.1 10.2-3.7 15.2-1.6zM160 256a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zm224 0a128 128 0 1 0 -256 0 128 128 0 1 0 256 0z"/></svg>`
   }
 
   var primaryLightMode = scriptTag.getAttribute("is-primary-light");
   if (!primaryLightMode) {
     console.warn(`Value of is-primary-light not found. Setting to "true" (Variable should be set to "true" if light theme is primary or "false" if dark theme is primary)`);
     primaryLightMode = "true"
-    return;
+
   }
   let primaryColors = {};
   let secondaryColors = {};
@@ -77,7 +73,7 @@ function colorModeToggle() {
     let secondaryValue = computed.getPropertyValue(`--${secondaryTheme}--${item}`);
     if (primaryValue.length) {
       if (!secondaryValue.length) secondaryValue = primaryValue;
-      
+
       primaryColors[`--${primaryTheme}--${item}`] = primaryValue;
       secondaryColors[`--${primaryTheme}--${item}`] = secondaryValue;
     }
@@ -100,6 +96,17 @@ function colorModeToggle() {
         htmlElement.style.setProperty(key, colorObject[key]);
       });
     }
+    //Set the svg logo on the toggle button.
+    if (colorObject === secondaryColors) {
+      document.querySelectorAll(`.${toggleButton}`).forEach((el) => {
+        el.innerHTML = secondaryLogo
+      })
+    }
+    else {
+      document.querySelectorAll(`.${toggleButton}`).forEach((el) => {
+        el.innerHTML = primaryLogo
+      })
+    }
   }
 
   function goSecondary(secondary, animate) {
@@ -108,13 +115,13 @@ function colorModeToggle() {
       htmlElement.classList.add("secondary-mode");
       setColors(secondaryColors, animate);
       togglePressed = "true";
-      $(`.${toggleButton}`).html(secondaryLogo)
+
     } else {
       localStorage.setItem("secondary-mode", "false");
       htmlElement.classList.remove("secondary-mode");
       setColors(primaryColors, animate);
       togglePressed = "false";
-      $(`.${toggleButton}`).html(primaryLogo)
+
     }
     if (typeof toggleEl !== "undefined") {
       toggleEl.forEach(function (element) {
@@ -127,7 +134,7 @@ function colorModeToggle() {
     goSecondary(e.matches, false);
   }
   var colorPreference
-  if(primaryLightMode === "true") {
+  if (primaryLightMode === "true") {
     colorPreference = window.matchMedia("(prefers-color-scheme: dark)");
     colorPreference.addEventListener("change", (e) => {
       checkPreference(e);
@@ -155,6 +162,7 @@ function colorModeToggle() {
       element.setAttribute("role", "button");
       element.setAttribute("aria-pressed", togglePressed);
     });
+
     toggleEl.forEach(function (element) {
       element.addEventListener("click", function () {
         let secondaryClass = htmlElement.classList.contains("secondary-mode");
